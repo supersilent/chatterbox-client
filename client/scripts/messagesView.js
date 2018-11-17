b4e1d1d1a9868478e689bc36f5ace4a5b9bda1ff
supersilent <shrikethrush@gmail.com>
2018-11-16 20:06:48 -0800

    Add code to prevent XSS attacks

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -1,22 +1,27 @@
 var MessagesView = {
   $chats: $("#chats"),
 
-  initialize: function() {
-    setTimeout(this.render,1000);
-    // Parse.readAll(() => {
-    //   MessagesView.render();
-    // });
+  initialize: function () {
+    setTimeout(this.render, 1000);
   },
 
-  render: function() {
+  render: function () {
     for (const message of Messages.results) {
       MessagesView.renderMessage(message);
     }
   },
-  
-  renderMessage: function(message) {
+
+  renderMessage: function (message) {
+    let text;
+    if (message.text) {
+      text = message.text;
+      text = text.replace(/</g, "&lt;").replace(/\>/g, '&#62;');
+    } else {
+      let text = '';
+    }
     $("#chats").append(
-      `<p><b>${message.username}</b><br>${message.text}</p>`
+      `<p><b class='username'>${message.username}</b><br>${text}</p>`
     );
+    
   }
 };
